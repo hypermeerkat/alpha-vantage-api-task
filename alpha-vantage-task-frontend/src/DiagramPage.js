ea5073125609316b9f6ff38f0b30b8d10ddb9658
Andrew <andrewc.london2019a@gmail.com>
2024-08-06 19:03:13 +0100

    fix for diagram page

--- alpha-vantage-task-frontend/src/DiagramPage.js
+++ alpha-vantage-task-frontend/src/DiagramPage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function DiagramPage() {
+  const [diagramContent, setDiagramContent] = useState('');
+
+  useEffect(() => {
+    fetch('/large-scale-video-streaming-site.drawio.html')
+      .then(response => response.text())
+      .then(data => setDiagramContent(data))
+      .catch(error => console.error('Error loading diagram:', error));
+  }, []);
+
   return (
     <div>
       <h1>Large Scale Video Streaming Site Diagram</h1>
-      <iframe src="/large-scale-video-streaming-site.drawio.html" width="100%" height="600px" />
+      <div dangerouslySetInnerHTML={{ __html: diagramContent }} />
       <Link to="/">
         <button>Back to Home</button>
       </Link>
